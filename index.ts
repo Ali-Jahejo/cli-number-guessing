b3998166fe97467782c6bd5392f2eb2568d1dd66
Ali-Jahejo <m.alijahejo1@gmail.com>
2024-04-07 12:14:09 -0800

    New Version

--- index.ts
+++ index.ts
@@ -1,18 +1,42 @@
 #! /usr/bin/env node
 
 import inquirer from "inquirer";
+import chalk from "chalk";
 
+console.log(chalk.bold.underline.bgYellow.blue("\n***** WELCOME TO NUMBER GUESSING GAME *****\n"))
+
+let condition = true;
+
+while(condition){
 const randomNumber = Math.floor(Math.random() * 6 + 1);
 const answer = await inquirer.prompt([
   {
     name: "userGuessedNumber",
     type: "number",
-    message: "Please guess a number between 1-6: ",
+    message: chalk.green ("Please Guess A Number Between 1-6: "),
   },
 ]);
 
 if (answer.userGuessedNumber === randomNumber) {
-  console.log("congratulations! you guessed the right number");
+  console.log(chalk.bold.magenta("\n CONGRATULATIONS! You Guessed The Right number\n"));
 } else {
-  console.log("you guessed a wrong number please Try again");
+  console.log(chalk.red.bold(`\n You Guessed The Wrong Number.\n The Right Number Was "${randomNumber}"\n Please Try Again\n`));
+}
+
+let choice = await inquirer.prompt([
+  {
+    name: "whatToDo",
+    type: "confirm",
+    message: chalk.green("Wanna Try Again?"),
+    default: "false",
+  }
+])
+
+if (choice.whatToDo === true){
+  condition = true
+}
+else if(choice.whatToDo === false){
+  condition = false
+}
+
 }
